1f7775fb72e35a7639654140c3ace38ba3567497
7wen <995776164@qq.com>
2020-10-28 21:39:50 +0800

    添加了一个注释

--- src/main/java/GitHello.java
+++ src/main/java/GitHello.java
@@ -1,5 +1,6 @@
 public class GitHello {
     public static void main(String[] args) {
+        //添加一个注解
         System.out.println("hello world");
         System.out.println("afasf");
         System.out.println("hhahah");
